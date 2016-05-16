b14c8e798b09131da68c883af3ebb756395bc07b
srodrigo <sergio.rodrigo.royo@gmail.com>
2016-05-16 15:17:49 +0100

    Got rid of the enum, it's not really needed

--- src/main/kotlin/bowlingkata/BowlingGame.kt
+++ src/main/kotlin/bowlingkata/BowlingGame.kt
@@ -1,13 +1,14 @@
 package bowlingkata
 
-import bowlingkata.ScoreType.*
-
 private const val numNormalThrows = 18
+private const val MISS = '-'
+private const val SPARE = '/'
+private const val STRIKE = 'X'
 
 fun score(allThrows: String): Int =
         allThrows.foldIndexed(0) { idx, acc, aThrow ->
             var score = aThrow.toScore() + bonuses(aThrow, idx, allThrows)
-            if (aThrow.scoreType() == SPARE) {
+            if (aThrow == SPARE) {
                 score -= allThrows.previous(idx).toScore()
             }
             score + acc
@@ -17,12 +18,12 @@ private fun bonuses(aThrow: Char, idx: Int, allThrows: String): Int =
         if (isLastFrame(allThrows, idx)) 0
         else {
             val nextThrow = allThrows.next(idx)
-            when (aThrow.scoreType()) {
+            when (aThrow) {
                 SPARE -> nextThrow.toScore()
                 STRIKE -> {
                     val nextThrowButOne = allThrows.next(idx + 1)
                     var score = nextThrow.toScore() + nextThrowButOne.toScore()
-                    if (nextThrowButOne.scoreType() == SPARE) {
+                    if (nextThrowButOne == SPARE) {
                         score -= nextThrow.toScore()
                     }
                     score
@@ -34,33 +35,18 @@ private fun bonuses(aThrow: Char, idx: Int, allThrows: String): Int =
 private fun isLastFrame(allThrows: String, idx: Int): Boolean =
         numNormalThrows <= allThrows.substring(0, idx).sumBy { it.countThrows() }
 
-private fun Char.scoreType(): ScoreType =
-        when (this) {
-            in ('1'..'9') -> BALLS
-            '/' -> SPARE
-            'X' -> STRIKE
-            else -> MISS
-        }
-
-enum class ScoreType {
-    MISS,
-    BALLS,
-    SPARE,
-    STRIKE
-}
-
 private fun Char.toScore(): Int =
-        when (scoreType()) {
-            BALLS -> String(charArrayOf(this)).toInt()
+        when (this) {
+            MISS -> 0
             SPARE, STRIKE -> 10
-            else -> 0
+            else -> String(charArrayOf(this)).toInt()
         }
 
 private fun Char.countThrows(): Int =
-        if (scoreType() == STRIKE) 2 else 1
+        if (this == STRIKE) 2 else 1
 
 private fun String.previous(idx: Int): Char =
-        if (idx > 0) this[idx - 1] else '_'
+        if (idx > 0) this[idx - 1] else '-'
 
 private fun String.next(idx: Int): Char =
-        if (idx < length - 1) this[idx + 1] else '_'
+        if (idx < length - 1) this[idx + 1] else '-'
