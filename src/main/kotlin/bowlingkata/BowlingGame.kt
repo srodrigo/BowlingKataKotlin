911a2766c8eb4a4906c66232e17a065b11baf5d1
srodrigo <sergio.rodrigo.royo@gmail.com>
2016-05-16 17:31:37 +0100

    Converted some functions to extension functions

--- src/main/kotlin/bowlingkata/BowlingGame.kt
+++ src/main/kotlin/bowlingkata/BowlingGame.kt
@@ -7,28 +7,22 @@ private const val STRIKE = 'X'
 
 fun score(allRolls: String): Int =
         allRolls.foldIndexed(0) { idx, acc, roll ->
-            acc + roll.score() + bonus(allRolls, roll, idx) - spareDifference(allRolls, roll, idx)
+            acc + roll.score() + bonus(allRolls, roll, idx) - allRolls.spareDifference(roll, idx)
         }
 
 private fun bonus(allRolls: String, roll: Char, idx: Int): Int =
-        if (isLastFrame(allRolls, idx)) 0
+        if (allRolls.isLastFrame(idx)) 0
         else {
             val nextRoll = allRolls.next(idx)
             when (roll) {
                 SPARE -> nextRoll.score()
                 STRIKE -> {
                     val nextRollButOne = allRolls.next(idx + 1)
-                    nextRoll.score() + nextRollButOne.score() - spareDifference(allRolls, nextRollButOne, idx + 2)
+                    nextRoll.score() + nextRollButOne.score() - allRolls.spareDifference(nextRollButOne, idx + 2)
                 }
                 else -> 0
             }
         }
-
-private fun spareDifference(allRolls: String, roll: Char, idx: Int): Int =
-        if (roll == SPARE) allRolls.previous(idx).score() else 0
-
-private fun isLastFrame(allRolls: String, idx: Int): Boolean =
-        NUM_NORMAL_THROWS <= allRolls.substring(0, idx).sumBy { it.countRolls() }
 
 private fun Char.score(): Int =
         when (this) {
@@ -45,3 +39,9 @@ private fun String.previous(idx: Int): Char =
 
 private fun String.next(idx: Int): Char =
         if (idx < length - 1) this[idx + 1] else '-'
+
+private fun String.spareDifference(roll: Char, idx: Int): Int =
+        if (roll == SPARE) this.previous(idx).score() else 0
+
+private fun String.isLastFrame(idx: Int): Boolean =
+        NUM_NORMAL_THROWS <= this.substring(0, idx).sumBy { it.countRolls() }
